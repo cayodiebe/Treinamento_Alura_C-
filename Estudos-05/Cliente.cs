d67592022e99587daf4cdd49c902de202e816beb
Cayo Diebe <cayo.diebe@konia.com.br>
2019-11-20 14:30:28 -0300

    Aula 04

---
+++ Estudos-05/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estudos_05
+{
+    public class Cliente
+    {
+        public string nome;
+        public string cpf;
+        public string profissao;
+    }
+}
---
+++ Estudos-05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Estudos_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente gabriela = new Cliente();
+            //gabriela.nome = "Gabriela";
+            //gabriela.cpf = "40521304120";
+            //gabriela.profissao = "Desenvolvedora";
+
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = new Cliente();
+            conta.titular.cpf = "4051233213";
+            conta.titular.profissao = "Desenvolvedora C#";
+            Console.WriteLine(conta.titular.nome);
+            Console.WriteLine(conta.titular.cpf);
+            Console.WriteLine(conta.titular.profissao);
+            //conta.titular = gabriela;
+            conta.numero = 1234;
+            conta.saldo = 500;
+            conta.agencia = 13214;
+
+
+            conta.titular.nome = "Gabriela Costa";
+
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
