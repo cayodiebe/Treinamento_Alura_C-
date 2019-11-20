41d96b75779c51c2e0d78397a1e1e107d6b0f6eb
Cayo Diebe <cayo.diebe@konia.com.br>
2019-11-20 14:52:59 -0300

    Aula 05

---
+++ Estudos-06/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estudos_06
+{
+    public class Cliente
+    {
+        private string _cpf;
+
+        public string Nome { get; set; }
+        public string Cpf { 
+            
+            get { return _cpf; } 
+            set {}
+        }
+        public string Profissao { get; set; }
+    }
+}
---
+++ Estudos-06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Estudos_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Cayo";
+            cliente.Cpf = "3219813221";
+            cliente.Profissao = "Desenvolvedor";
+
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            conta.Saldo = -10;
+
+
+
+            Console.WriteLine(conta.Saldo);
+            Console.ReadLine() ;
+        }
+    }
+}
